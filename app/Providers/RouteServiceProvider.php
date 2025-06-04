a31b93f756ce97e680cf16feff011c7e54000509
UlfaLadayya <ulfaladayya123@gmail.com>
2025-06-04 12:07:51 +0700

    Fix route

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/homepageuser';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
